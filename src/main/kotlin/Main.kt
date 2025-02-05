15c35dbe3edd254c69549d9456f490fc777f1e81
Baslim <nikita7343072@gmail.com>
2025-02-05 12:30:18 +0500

    KS-5-8 реализовал парсер цитат

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,23 @@
-package org.example
+import org.jsoup.Jsoup
 
 fun main() {
-    println("Hello World!")
+
+    val url = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
+
+    val doc = Jsoup.connect(url).get()
+    val quoteContainers = doc.select("div.sc-2aegk7-2.bzpNIu")
+
+    if (quoteContainers.isEmpty()) {
+        println("Цитаты не найдены. Проверьте актуальность селектора.")
+        return
+    }
+
+    quoteContainers.forEachIndexed { index, container ->
+        val quoteText = container.text().trim().replace(Regex("\\s+"), " ")
+
+        println("Цитата ${index + 1}:")
+        println(quoteText)
+        println("―".repeat(30))
+    }
+
 }
