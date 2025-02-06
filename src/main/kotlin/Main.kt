62242a965747f54bb28080526981d462ba3a75a3
Baslim <nikita7343072@gmail.com>
2025-02-06 09:54:07 +0500

    KS-5-8 поменял тег на article в соответствии с замечанием

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,13 @@
 import org.jsoup.Jsoup
 
+const val QUOTE_SELECTOR = "article.sc-14uz67c-0.ccmjFA"
+
 fun main() {
 
     val url = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
 
     val doc = Jsoup.connect(url).get()
-    val articles = doc.select("div.sc-2aegk7-2.bzpNIu")
+    val articles = doc.select(QUOTE_SELECTOR)
 
     if (articles.isEmpty()) {
         println("Цитаты не найдены. Проверьте актуальность селектора.")
@@ -13,7 +15,7 @@ fun main() {
     }
 
     articles.forEachIndexed { index, article ->
-        val quoteText = article.select("div.sc-2aegk7-2.bzpNIu").text().trim().replace(Regex("\\s+"), " ")
+        val quoteText = article.text().trim().replace(Regex("\\s+"), " ")
 
         println("Цитата ${index + 1}:")
         println(quoteText)
