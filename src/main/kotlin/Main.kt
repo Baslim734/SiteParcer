cac4cf2ca840b704cabee4cdbd49b1600ccda788
Baslim <nikita7343072@gmail.com>
2025-02-06 09:46:30 +0500

    KS-5-8 поменял тег на article в соответствии с замечанием

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,19 +5,20 @@ fun main() {
     val url = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
 
     val doc = Jsoup.connect(url).get()
-    val quoteContainers = doc.select("div.sc-2aegk7-2.bzpNIu")
+    val articles = doc.select("div.sc-2aegk7-2.bzpNIu")
 
-    if (quoteContainers.isEmpty()) {
+    if (articles.isEmpty()) {
         println("Цитаты не найдены. Проверьте актуальность селектора.")
         return
     }
 
-    quoteContainers.forEachIndexed { index, container ->
-        val quoteText = container.text().trim().replace(Regex("\\s+"), " ")
+    articles.forEachIndexed { index, article ->
+        val quoteText = article.select("div.sc-2aegk7-2.bzpNIu").text().trim().replace(Regex("\\s+"), " ")
 
         println("Цитата ${index + 1}:")
         println(quoteText)
         println("―".repeat(30))
+
     }
 
 }
